d39fd17f5f5cb258419d29bc67af1f3a802bd7b0
Wahidullah Rahimi <wahidullahrahimi@wless-user-172019130251.temp.wsu.edu>
2023-03-03 17:35:35 -0800

    I am rich app

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Wahidullah Rahimi on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
